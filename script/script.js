287a71785932c819b69ea1e512f93333cc20da29
DEN <example@gmail.com>
2021-09-19 12:09:45 +0200

    pervoje sohranenije

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// let person = {
+//     name: "Den",
+//     age: 34,
+//     isMarried: false
+// };
+
+
+// let answer = +prompt ("Are you +18?" , "YES");
+// console.log(typeof(answer));
+
+let incr = 100;
+let less = 10;
+
+incr++;
+less--;
+// console.log(incr);
+// console.log(less);
+
+
+
+
+let isChecked = true,
+    isFalse = true;
+    console.log(typeof(incr) )
